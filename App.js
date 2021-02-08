49180375c307ab39dd5710226fb3995e1c2dfd09
CoderJuan21 <70552194+CoderJuan21@users.noreply.github.com>
2021-02-08 15:30:05 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUpLoginScreen from './screens/SignUpLoginScreen';
+import ExchangeScreen from './screens/ExchangeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {AppTabNavigator} from './components/AppTabNavigator'
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
+
+export default function App() {
+  return (
+    
+    <SignUpLoginScreen/>
+   
+  )
+}
+const switchNavigator=createSwitchNavigator({
+  ExchangeScreen:{screen:ExchangeScreen},
+  HomeScreen:{screen:HomeScreen},
+})
+
+const AppContainer=createAppContainer(switchNavigator)
